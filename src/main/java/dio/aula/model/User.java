15048b16486ce49ad297484197d9e7662e7063f5
antonioalvs <antonio.aa.alvs@gmail.com>
2024-09-05 12:18:52 -0300

    start

---
+++ src/main/java/dio/aula/model/User.java
@@ -0,0 +1,68 @@
+package dio.aula.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(name = "usuario",length = 50, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String username;
+    @Column(length = 100, nullable = false)
+    private String password;
+    
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return
+            "id='" + getId() + "'\n" +
+            "name='" + getName() + "'\n" +
+            "username='" + getUsername() + "'\n" +
+            "password='" + getPassword() + "'\n";
+    }
+    
+
+}
